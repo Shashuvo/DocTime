b9852d56c410b879a69a96401fae0e673d0723e7
Shashuvo <shahariat2810@gmail.com>
2025-09-12 00:01:57 +0600

    Doctor Details and Appointment button added

--- src/Pages/DoctorDetails/DoctorDetails.jsx
+++ src/Pages/DoctorDetails/DoctorDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PiTrademarkRegisteredLight } from 'react-icons/pi';
 import { useLoaderData, useParams } from 'react-router';
 import AvailableDay from '../../Components/AvailableDay/AvailableDay';
+import { MdErrorOutline } from 'react-icons/md';
 
 const DoctorDetails = () => {
     const { registration_number } = useParams();
@@ -9,6 +10,13 @@ const DoctorDetails = () => {
     console.log(registration_number, data);
     const showDoctor = data.find((doctor) => doctor.registration_number === registration_number);
     const { image, name, education, speciality, hospital, available_days, fee } = showDoctor;
+
+    const today = new Date().toLocaleString('en-US', { weekday: 'long' });
+    const isAvailable = available_days.includes(today);
+
+
+    
+
     return (
         <div className='w-4/5 mx-auto mb-[78px]'>
             {/* profile details */}
@@ -17,7 +25,7 @@ const DoctorDetails = () => {
                 <p className='text-center font-medium opacity-70 pb-[40px]'>Healing with Trust, Serving with Care.</p>
             </div>
             {/* doctors details */}
-            <div className="hero bg-white rounded-3xl ">
+            <div className="hero bg-white rounded-3xl mb-6 ">
                 <div className="hero-content flex flex-col lg:flex-row lg:items-stretch justify-between lg:justify-start lg:mx-[56px] gap-3 lg:gap-6 lg:py-[56px]">
                     {/* image container */}
                     <div className="lg:w-1/3 lg:h-auto flex">
@@ -51,8 +59,27 @@ const DoctorDetails = () => {
                 </div>
             </div>
             {/* book appointment */}
-            <div>
-
+            <div className=' bg-white rounded-2xl'>
+                <h1 className='text-center pt-8 font-extrabold text-2xl'>Book an Appointment</h1>
+                <div className='w-11/12 mx-auto'>
+                    <div className=' border-b-2 border-dashed border-black/20 w-full my-4'></div>
+                    <div className='flex justify-between items-center'>
+                        <h3 className='font-bold text-[18px]'>Availability</h3>
+                        <p className={`badge font-medium text-[12px] md:text-[14px] rounded-4xl py-[14px] px-3 ${isAvailable ? "text-[#09982F] bg-[#09982F]/10 border-[#09982F]/20" : "text-[#982F09] bg-[#982F09]/10 border-[#982F09]/20"}`}>
+                            {isAvailable ? 'Doctor Available Today' : 'Doctor Not Available Today'}
+                        </p>
+                    </div>
+                    <div className=' border-b-2 border-black/20 w-full my-4'></div>
+                    <div className=' '>
+                        <p className='font-medium flex flex-col text-center justify-center items-center gap-2 bg-[#FFA000]/10 px-1 py-2 rounded-3xl text-[#FFA000] md:flex-row'><MdErrorOutline size={20}></MdErrorOutline>
+                            {isAvailable ? "Due to high patient volume, we are currently accepting appointments for today only. We appreciate your understanding and cooperation." :
+                                "Due to high patient volume, we are not currently accepting appointments for today. We appreciate your understanding and cooperation."}
+                        </p>
+                    </div>
+                    <div className='w-full md:w-11/12 mx-auto mt-[36px] pb-[56px]'>
+                        <button disabled={!isAvailable} className={`btn btn-outline py-6 w-full btn-primary rounded-4xl text-[20px]  font-bold ${isAvailable ? 'text-white bg-[#176AE5] hover:border-2 hover:border-[#176AE5] hover:bg-white hover:text-[#176AE5]' : "Cursor Not Available"} `}>Book Appointment Now</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
